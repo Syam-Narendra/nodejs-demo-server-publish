afb3c1911252c96b654da1251a61c45c4f711a1f
Syam <syamnarendra85@gmail.com>
2023-10-16 19:53:42 +0530

    First

---
+++ demo_server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.send("Work")
+})
+
+app.listen(3000,()=>{
+    console.log("3000 Running")
+})
